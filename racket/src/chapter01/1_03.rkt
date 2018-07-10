d84b08cb8e2d58d7350fd5252c4f659ca28089a7
Naumenko Nikita <naumenkoniki@gmail.com>
2018-07-10 20:19:43 +0300

    add 1_10 task

--- racket/src/chapter01/1_03.rkt
+++ racket/src/chapter01/1_03.rkt
@@ -15,7 +15,3 @@
            (> c b)
            (* a c)))
         ))
-
-(check-equal? (sum-of-max 3 2 1) 6);6
-(check-equal? (sum-of-max 3 6 8) 48);48
-(check-equal? (sum-of-max 9 6 8) 72);72
---
+++ racket/test/chapter01/1_10.rkt
@@ -0,0 +1,7 @@
+#lang racket
+(require rackunit)
+(require "../../src/chapter01/1_10.rkt")
+
+(check-equal? (A 1 10) 1024)
+(check-equal? (A 2 4) 65536)
+(check-equal? (A 3 3) 65536)
