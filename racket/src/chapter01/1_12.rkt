b41e7d5c262eb1d574f6603575010c366d41aaa1
Naumenko Nikita <naumenkoniki@gmail.com>
2018-07-14 11:42:12 +0300

    Add 1_12 task

---
+++ racket/src/chapter01/1_12.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(provide pascal)
+
+(define (pascal element row)
+  (write row)
+  (cond ((or (= element 1) (= element row)) 1)
+        ((and (> element 1) (> row element)) (+ (pascal (- element 1) (- row 1))
+                                                (pascal element (- row 1))))))
