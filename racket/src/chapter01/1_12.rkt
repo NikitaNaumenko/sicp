b60ba050f5b73021d69c413ca06d7bfbbaac45a6
Naumenko Nikita <naumenkoniki@gmail.com>
2018-07-14 17:42:15 +0300

    Add doc for 1_12 exercise

--- racket/src/chapter01/1_12.rkt
+++ racket/src/chapter01/1_12.rkt
@@ -2,7 +2,6 @@
 (provide pascal)
 
 (define (pascal element row)
-  (write row)
   (cond ((or (= element 1) (= element row)) 1)
         ((and (> element 1) (> row element)) (+ (pascal (- element 1) (- row 1))
                                                 (pascal element (- row 1))))))
