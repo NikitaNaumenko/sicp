34f735f1928ad0f7535764f3ed85a90aac460d01
Naumenko Nikita <naumenkoniki@gmail.com>
2018-09-13 20:40:37 +0300

    Add 1.17 task

---
+++ racket/src/chapter01/1_17.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(provide double halve fast-mult fast-mult-second)
+
+(define (halve x) (/ x 2))
+
+(define (double x) (* x 2))
+
+(define (fast-mult b n)
+  (define (even? n) (= (remainder n 2) 0))
+  (define (zero? n) (= n 0))
+  (define (dec n) (- n 1))
+  (define (inc n) (+ n 1))
+  (define (positive? n) (> n 0))
+  (cond ((zero? b) 0)
+        ((even? b) (fast-mult (halve b) (double n)))
+        ((positive? b) (+ (fast-mult (halve (dec b)) (double n))
+                                      n))
+        (else (- (fast-mult (halve (inc b)) (double n))
+                            n))))
+
+(define (fast-mult-second a n) 
+  (define (even? n) (= (remainder n 2) 0))
+  (define (zero? n) (= n 0))
+  (define (dec n) (- n 1))
+  (cond ((zero? n) 0)
+        ((even? n) (double (fast-mult-second a (halve n))))
+        (else (+ a (fast-mult-second a (dec n))))))
